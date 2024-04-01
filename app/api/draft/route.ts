9f09a83809caa330d1cfdfad54b339da915d6494
Arseniy Kapshtyk <kapshtykas@yandex.ru>
2024-04-01 16:40:42 +0300

    Refactor draft route handler to use environment variables for URLs

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -1,35 +1,24 @@
-// route handler with secret and slug
 import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { getPostBySlug } from "@/lib/api/posts";
 
 export async function GET(request: Request) {
-  // Parse query string parameters
   const { searchParams } = new URL(request.url);
   const secret = searchParams.get("secret");
   const slug = searchParams.get("slug");
 
-  // Check the secret and next parameters
-  // This secret should only be known to this route handler and the CMS
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !slug) {
     return new Response("Invalid token", { status: 401 });
   }
 
-  // Fetch the headless CMS to check if the provided `slug` exists
-  // getPostBySlug would implement the required fetching logic to the headless CMS
   const post = await getPostBySlug(slug, true);
-  console.log(post);
 
-  // If the slug doesn't exist prevent draft mode from being enabled
   if (!post) {
     return new Response("Invalid slug", { status: 401 });
   } else {
-    // Enable Draft Mode by setting the cookie
     draftMode().enable();
 
-    // Redirect to the path from the fetched post
-    // We don't redirect to searchParams.slug as that might lead to open redirect vulnerabilities
-    redirect("https://contentful-nextjs-psi.vercel.app/posts/" + post.slug);
+    redirect(`${process.env.NEXT_PRODUCTION_URL}/posts/${post.slug}`);
   }
 }
