285bca9243e8294a656b3b363596297f8874bb36
Arseniy Kapshtyk <kapshtykas@yandex.ru>
2024-05-23 14:48:18 +0300

    chore: Update middleware to include a new config for URL matching

--- middleware.ts
+++ middleware.ts
@@ -6,3 +6,7 @@ export function defaultMiddleware() {
   return NextResponse.next();
 }
 export default withInternationalisation(defaultMiddleware);
+
+export const config = {
+  matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+};
